e71ae0321f4329896a9e82c0f36294aa6ed323df
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2022-01-23 22:54:36 +0900

    캘린더 일정 추가 (#36)
    * calendar view
    
    * 캘린더 추가 디비 연동
    
    * 캘린더 일정추가 구현

--- src/main/java/com/project/nadaum/calendar/controller/CalendarController.java
+++ src/main/java/com/project/nadaum/calendar/controller/CalendarController.java
@@ -1,10 +1,12 @@
 package com.project.nadaum.calendar.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.nadaum.calendar.model.service.CalendarService;
@@ -35,7 +38,6 @@ public class CalendarController {
 	@ResponseBody
 	@GetMapping("/calendarList.do")
 	public List<Calendar> calendarList(
-			HttpServletRequest resp, 
 			Model model, 
 			String id) {
 		List<Calendar> list = calendarService.calendarList(id);
@@ -47,16 +49,23 @@ public class CalendarController {
 	}
 
 	// 달력데이터 추가
-	//@ResponseBody
+	@ResponseBody
 	@PostMapping("/addCalendar.do")
-	public String addCalendar(@RequestBody Map<String, Object> param) {
-		System.out.println(String.valueOf(param.get("id")));
-		JSONObject jsonObject = null;
+	public Map<String, Object> addCalendar(@RequestBody Map<String, Object> eventData) {
+		Map<String, Object> map = new HashMap<String, Object>();	
+		map.put("id", eventData.get("id"));
+		map.put("title", eventData.get("title"));
+		map.put("startDate", eventData.get("start"));
+		map.put("endDate", eventData.get("end"));
+		map.put("content", eventData.get("description"));
+		map.put("type", eventData.get("type"));
+		map.put("backgroundColor", eventData.get("backgroundColor"));
+		map.put("textColor", eventData.get("textColor"));
+		map.put("allDay", eventData.get("allDay"));
+		System.out.println(map);
+		int result = calendarService.addCalendar(map);
+		return map;
 		
-		jsonObject = new JSONObject();
-		jsonObject.put("id", String.valueOf(param.get("id")));
-		log.debug("param = " + param);		
-		return jsonObject.toString();	
 	}
 	
 	
