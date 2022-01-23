825d657fd8adc80485613818f6761a0531485575
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2022-01-23 17:52:27 +0900

    캘린더 추가 (#32)
    * calendar view
    
    * 캘린더 추가 디비 연동

---
+++ src/main/java/com/project/nadaum/calendar/model/service/CalendarServiceImpl.java
@@ -0,0 +1,32 @@
+package com.project.nadaum.calendar.model.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.nadaum.calendar.model.dao.CalendarDao;
+import com.project.nadaum.calendar.model.vo.Calendar;
+
+@Service
+public class CalendarServiceImpl implements CalendarService {
+	
+	@Autowired
+	private CalendarDao calendarDao;
+
+
+	@Override
+	public List<Calendar> calendarList(String id) {
+		return calendarDao.calendarList(id);
+	}
+
+
+	@Override
+	public int addCalendar(Calendar calendar) {
+		return calendarDao.addCalendar(calendar);
+	}
+
+}
