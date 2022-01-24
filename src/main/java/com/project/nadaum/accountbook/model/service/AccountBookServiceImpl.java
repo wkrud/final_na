811c8d2e004ecb0b34ace1bea1e5928e05f17547
hyemiL <89233243+hyemiL@users.noreply.github.com>
2022-01-24 22:24:59 +0900

    Hyemi.millie (#46)
    * insert
    
    * db에 내역 insert
    
    * list
    
    * 모달 수정
    
    * id 연동
    
    * 사용자별 가계부 리스트 출력
    
    * income / expense 조회
    
    * .
    
    * 가계부에 입력한 자산 연동
    
    * 수입/지출 필터링
    
    * 가계부에 등록한 내역 연동
    
    Co-authored-by: hyemiL <hyemi.millie@gamil.com>

--- src/main/java/com/project/nadaum/accountbook/model/service/AccountBookServiceImpl.java
+++ src/main/java/com/project/nadaum/accountbook/model/service/AccountBookServiceImpl.java
@@ -1,5 +1,52 @@
 package com.project.nadaum.accountbook.model.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.nadaum.accountbook.model.dao.AccountBookDao;
+import com.project.nadaum.accountbook.model.vo.AccountBook;
+
+@Service
 public class AccountBookServiceImpl implements AccountBookService {
+	
+	@Autowired
+	private AccountBookDao accountBookDao;
+
+	@Override
+	public int insertAccount(AccountBook account) {
+		return accountBookDao.insertAccount(account);
+	}
+
+	@Override
+	public List<AccountBook> selectAllAccountList(String id) {
+		return accountBookDao.selectAllAccountList(id);
+	}
+
+	@Override
+	public int deleteAccount(String code) {
+		return accountBookDao.deleteAccount(code);
+	}
+
+	@Override
+	public List<AccountBook> monthlyTotalIncome(String id) {
+		return accountBookDao.monthlyTotalIncome(id);
+	}
+
+	@Override
+	public String monthlyAccount(String id) {
+		return accountBookDao.monthlyAccount(id);
+	}
+
+	@Override
+	public Map<String, String> income_expense_filter(String id, String income_expense) {
+		return accountBookDao.income_expense_filter(id, income_expense);
+	}
+	
+	
+	
+	
 
 }
--- src/main/webapp/resources/js/accountbook/main.js
+++ src/main/webapp/resources/js/accountbook/main.js
@@ -2,6 +2,7 @@
  * 
  */
  
+ 
  /* 검색 카테고리 js*/
  
 /* $(() => {
@@ -36,11 +37,160 @@
 });*/
 
 
-
 /*버튼 테스트*/
 
-$('#btn1').on('click', function() {
-	    // 모달창 띄우기
-	    console.log("찍히냐???????");
+$("#closeBtn").on('click', function(){
+	$(".modal-background").fadeOut();
+});
+	
+$("#btn1").on('click', function(){
+	$(".modal-background").fadeIn();
+});
+
+  	//ajax 로딩시 필요한 값 변수 선언
+    var $id = $("#id").val();
+   	var $income = $("#income").val();
+	var $expense = $("#expense").val();
+	
+	//수입 지출 변환 함수
+	function IE(x) {
+		if(x == 'I')
+			return "수입";
+		else if(x == 'E')
+			return "지출";
+	}
+	
+	//날짜 기본값 세팅
+	window.onload = function() {
+		today = new Date();
+		today = today.toISOString().slice(0, 10);
+		bir = document.getElementById("reg_date");
+		bir.value = today;
+	}
+    
+   //가계부 리스트 조회
+ 	$.ajax({
+		url: "/nadaum/accountbook/selectAllAccountList.do",
+		type: "GET",
+		data: {
+			id : $id
+		},
+		dataType : "json",
+		contentType : "application/json; charset=UTF-8",
+		success : function(accountList){
+			console.log(accountList);
+			
+			$.each(accountList, function(i, account) {
+			var result = `
+				<tr>
+					<td rowspan="2">`+IE(`${account.incomeExpense}`)+`</td>
+					<td colspan="2">${account.regDate}</td>
+					<td>`+numberWithCommas(`${account.price}`)+`</td>
+				</tr>		
+				<tr>
+					<td>${account.detail}</td>
+					<td><button id="deleteBtn" onclick="deleteDetail();">삭제하기</button></td>	
+					<input type="hidden" name="code" value="${account.code}" />
+				</tr>
+				`
+			$('.account_list').append(result);
+			})
+			
+		},
+		error : function(data){
+			console.log(data);
+			console.log("어디가 문제일까,,,,,,,,,,,");
+		}	
+	});
+	
+	//삭제
+
+	
+	
+	//원화표시 정규식
+	function numberWithCommas(n) {
+    	return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
+	
+	//수입 지출 조회
+    $.ajax({
+		url: "/nadaum/accountbook/monthlyTotalIncome.do",
+		type: "GET",
+		data: {
+			id : $id
+		},
+		dataType : "json",
+		contentType : "application/json; charset=UTF-8",
+		success(incomeList){		
+			var result = `
+				<td>\\<span style="color:green">`+numberWithCommas(incomeList[1].total)+`</span></td>
+				<td>\\<span style="color:red">`+numberWithCommas(incomeList[0].total)+`</span></td>`	
+				
+			$('.user_income_expense').append(result);
+
+		}
+	});
+	
+    
+    //이달의 총 자산
+    $.ajax({
+		url: "/nadaum/accountbook/monthlyAccount.do",
+		type: "GET",
+		data: {
+			id : $id
+		},
+		dataType : "json",
+		contentType : "application/json; charset=UTF-8",
+		success(monthlyAccount){
+			var result = 
+					`<td colspan="2" style="font-size:40px">`+numberWithCommas(monthlyAccount)+`원 </td>`
+			$('#total_income').append(result);
+
+		}
 	});
 
+	
+	//수입 필터링
+	$('#income_filter_btn').click(function() {
+		$.ajax({
+			url : '/nadaum/accountbook/income_expense_filter.do',
+			type : "GET",
+			data : {
+				id : $id,
+				income_expense : $income
+			},
+			dataType : "json",
+			contentType : "application/json; charset=UTF-8",
+			success(incomeList) {
+				console.log($id);
+				console.log($income);
+				console.log(incomeList);
+			},
+			error(xhr, testStatus, err) {
+				console.log("error", xhr, testStatus, err);
+				alert("조회에 실패했습니다.");
+			}
+		});
+	});
+	
+	//지출
+	$('#expense_filter_btn').click(function() {
+		$.ajax({
+			url : '/nadaum/accountbook/income_expense_filter.do',
+			type : "GET",
+			data : {
+				id : $id,
+				income_expense : $expense
+			},
+			dataType : "json",
+			contentType : "application/json; charset=UTF-8",
+			success(incomeList) {
+				console.log($expense);
+				console.log(incomeList);
+			},
+			error(xhr, testStatus, err) {
+				console.log("error", xhr, testStatus, err);
+				alert("조회에 실패했습니다.");
+			}
+		});
+	});
