0291ccdb6f9d7a063f670d96deb03585143c5daa
mirae0312 <ekdtls58@gmail.com>
2022-02-03 19:28:52 +0900

    일기 등록 / 최근 목록 불러오기 부분 구현

--- src/main/java/com/project/nadaum/diary/model/dao/DiaryDao.java
+++ src/main/java/com/project/nadaum/diary/model/dao/DiaryDao.java
@@ -1,11 +1,16 @@
 package com.project.nadaum.diary.model.dao;
 
+import java.util.List;
 import java.util.Map;
+
+import com.project.nadaum.diary.model.vo.Diary;
 
 public interface DiaryDao {
 
 	int insertDiary(Map<String, Object> map);
 
 	int emotionNo(String emotion);
+
+	List<Diary> recentlyDiary(String id);
 
 }
